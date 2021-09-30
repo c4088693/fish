fef04d7f334e92fcf38316f1f734fe86f72627a3
kamlesh <ks6849663@gmail.com>
2021-09-30 23:10:37 +0530

    first commmit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello");
+
+echo "# fish" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ks6849/fish.git
+git push -u origin main
